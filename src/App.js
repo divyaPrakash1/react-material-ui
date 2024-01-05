a6c9a363c5cb126648bbae8203f544b350b4040a
Divya Prakash Mishra <iamdpm93@gmail.com>
2024-01-05 14:20:41 +0530

    Radio

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import './App.css';
 // import { Button, ButtonGroup, } from '@mui/material';
 // import DeleteIcon from '@mui/icons-material/Delete';
 // import SendIcon from '@mui/icons-material/Send';
+// import Checkbox from '@mui/material/Checkbox';
+// import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+// import Favorite from '@mui/icons-material/Favorite';
+import { Radio } from '@mui/material';
 import { useState } from 'react';
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
 
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 function App() {
-  const [color, setColor] = useState('primary')
-  const [disable, setDisable] = useState(false)
-  const [value, setValue] = useState([])
-  const customMe = () => {
-    // alert(123123)
-    setColor('secondary')
-    // setDisable(true)
-  }
-  const checkBoxFun = (e) => {
-    console.log('123123', e.target.value)
-    let name = value;
-    name.push(e.target.value);
-    console.log(name)
+  // const [color, setColor] = useState('primary')
+  // const [disable, setDisable] = useState(false)
+  // const [checkboxValue, setCheckboxValue] = useState([])
+  const [radioValue, setRadioValue] = useState('female')
+  // const customMe = () => {
+  //   // alert(123123)
+  //   setColor('secondary')
+  //   // setDisable(true)
+  // }
+  // const checkBoxFun = (e) => {
+  //   console.log('123123', e.target.value)
+  //   let name = checkboxValue;
+  //   name.push(e.target.value);
+  //   console.log(name)
+  // }
+  const checkRadio = (e) => {
+    setRadioValue(e.target.value);
+    console.log('aaaa', e.target.value);
   }
   return (
     <div className="App">
@@ -68,15 +74,26 @@ function App() {
           disabled={disable}
         >Three</Button>
       </ButtonGroup> */}
-      <Checkbox {...label} />
-
+      {/* <Checkbox {...label} />
       <Checkbox color='primary' value='10' onChange={(e) => checkBoxFun(e)} />
       <Checkbox color='primary' value='11' onChange={(e) => checkBoxFun(e)} />
       <Checkbox color="secondary" value='12' onChange={(e) => checkBoxFun(e)} />
       <Checkbox color='success' value='13' onChange={(e) => checkBoxFun(e)} />
       <Checkbox color='success' value='14' indeterminate onChange={(e) => checkBoxFun(e)} />
+      <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} color='secondary' /> */}
 
-      <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} color='secondary' />
+      <div>
+        <Radio color='primary' value='male' onChange={(e) => checkRadio(e)} checked={radioValue === 'male'} />
+        <span>Male</span>
+      </div>
+      <div>
+        <Radio color='primary' value='female' onChange={(e) => checkRadio(e)} checked={radioValue === 'female'} />
+        <span>Female</span>
+      </div>
+      <div>
+        <Radio color='primary' value='other' onChange={(e) => checkRadio(e)} checked={radioValue === 'other'} />
+        <span>Other</span>
+      </div>
 
     </div>
   );
