c4c2d938bd84839b5ea1ce5eeb395239881a3c50
Divya Mishra <divya.mishra@velocis.in>
2024-01-05 12:28:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import { useState } from 'react';
+
 function App() {
+  const [color, setColor] = useState('primary')
+  const [disable, setDisable] = useState(false)
+  const customMe = () => {
+    // alert(123123)
+    setColor('secondary')
+    setDisable(true)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React MUI</h1>
+      {/* <button>Click Me</button> */}
+      {/* <Button style={{ color: 'prime' }}>Material UI Button</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+      <Button variant="outlined" color="error">
+        Error
+      </Button>
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button> */}
+      <Button
+        color={color}
+        variant='contained'
+        onClick={() => customMe()}
+        disabled={disable}
+        startIcon={<DeleteIcon />}
+        endIcon={<SendIcon />}
+      >Click Me</Button>
     </div>
   );
 }
